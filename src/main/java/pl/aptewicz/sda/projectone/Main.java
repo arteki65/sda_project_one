133d1a9ff2aca61de9575580f5e24a7b3dd70461
Arkadiusz Aptewicz <Arkadiusz.Aptewicz@dbschenker.com>
2020-09-05 22:41:17 +0200

    app menu

--- src/main/java/pl/aptewicz/sda/projectone/Main.java
+++ src/main/java/pl/aptewicz/sda/projectone/Main.java
@@ -1,21 +1,71 @@
 package pl.aptewicz.sda.projectone;
 
-import pl.aptewicz.sda.projectone.service.http.OpenNotifyConnector;
 import pl.aptewicz.sda.projectone.service.formatter.JsonResponseFormatter;
+import pl.aptewicz.sda.projectone.service.http.OpenNotifyConnector;
 
 import java.net.http.HttpClient;
 import java.time.Duration;
+import java.util.Scanner;
 
 public class Main {
 
     private static final HttpClient httpClient =
             HttpClient.newBuilder().version(HttpClient.Version.HTTP_1_1).connectTimeout(Duration.ofSeconds(10)).build();
 
-    public static void main(String[] args) {
-        System.out.println("Hello javaLon4");
+    private static final Scanner keyboardScanner = new Scanner(System.in);
 
-        //        final var openNotifyConnector = new OpenNotifyConnector(HttpResponse::body, httpClient);
+    public static void main(String[] args) {
+        var programRunning = true;
+        while (programRunning) {
+            showMenu();
+            var chosenOption = keyboardScanner.nextLine();
+            switch (chosenOption) {
+                case "1":
+                    showPeopleInSpace();
+                    waitForUserInteraction();
+                    break;
+                case "2":
+                    showCurrentLocationOfISS();
+                    waitForUserInteraction();
+                    break;
+                case "3":
+                    programRunning = false;
+                    System.out.println("Good bye!");
+                    break;
+                default:
+                    showUnknownOperationInfo(chosenOption);
+            }
+        }
+    }
+
+    private static void showMenu() {
+        // @formatter:off
+        final var menu = "SDA javaLon4 project one!\n" +
+                "Choose menu option:\n" +
+                "1 - show people in space\n" +
+                "2 - show current location of ISS\n" +
+                "3 - exit";
+        // @formatter:on
+        System.out.println(menu);
+    }
+
+    private static void waitForUserInteraction() {
+        System.out.println("Press any key to continue...");
+        keyboardScanner.nextLine();
+    }
+
+    private static void showPeopleInSpace() {
         final var openNotifyConnector = new OpenNotifyConnector(new JsonResponseFormatter(), httpClient);
         System.out.println(openNotifyConnector.getPeopleInSpace());
+    }
+
+    private static void showCurrentLocationOfISS() {
+        System.out.println("Showing current location of ISS is not supported yet.");
+    }
+
+    private static void showUnknownOperationInfo(String chosenOption) {
+        final var unknownOperationInfo =
+                String.format("\"%s\" option is unknown. Please specify one of the menu " + "options!", chosenOption);
+        System.out.println(unknownOperationInfo);
     }
 }
